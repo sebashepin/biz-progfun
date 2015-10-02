6e9591f3525f63bfb5351f1f4fba49373e8e48a3
Sebastian <sebashepin@gmail.com>
2015-10-01 19:39:32 -0500

    sebastian > 1.2.2 Exercises

--- 1.2/sebastian/1.2.scm
+++ 1.2/sebastian/1.2.scm
@@ -55,64 +55,6 @@
 ; (g n) computes 2^n
 ; (g n) computes n∨2 (tetration, had to look that shit up)
 
-; =======================================================
-; Exercise 1.11 - Recursive/Iterative function
-; =======================================================
-
-; Recursive
-(define (f11 n) 
-  (cond ((< n 3)
-          n)
-         (else (+
-                 (f11 (- n 1))
-                 (* 2 (f11 (- n 2)))
-                 (* 3 (f11 (- n 3)))
-                 )
-               )
-         )
-   
-  )
-
-;Iterative
-(define (f11-iter-start n)
-  (f11-iter n 2 1 0)
-  )
-
-(define (f11-iter n n-1 n-2 n-3)
-  (let ((currentValue (+
-                        n-1
-                        (* 2 n-2)
-                        (* 3 n-3)
-                        )
-                      ))
-
-    (cond ((<= n 3) currentValue)
-          (else
-            (f11-iter 
-              (- n 1)
-              currentValue
-              n-1
-              n-2
-              )
-            )
-          )
-    )
-  )
-
-; =======================================================
-; Exercise 1.12 - Pascal's triangle
-; =======================================================
-
-(define (pascal row column)
-  (cond ((or (= column 1) (= column row)) 1)
-        (else (+ 
-                (pascal (- row 1) (- column 1))
-                (pascal (- row 1) column)
-                )
-              )
-        )
-  )
-
 ;Printing
 (define (printProc name function)
   (display newline)
