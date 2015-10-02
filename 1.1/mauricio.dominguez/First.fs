f40ddd8f1c35fb7998097f02884ae9ee41834280
evilpilaf <mauriciod73@gmail.com>
2015-10-01 23:24:19 -0500

    First commit
    Ejercicios de las 2 primeras clases

---
+++ 1.1/mauricio.dominguez/First.fs
@@ -0,0 +1,35 @@
+﻿namespace DefaultProgram
+
+module First =
+
+    let Point1_3_sum_squares x y z =
+        let square (x:float) = x*x
+        // 1.3
+        let squareSum x y = square x + square y
+        let larger x y = if x > y then x else y
+        let squareSumGreater x y z = 
+            if x > y then
+                squareSum x (larger y z)
+            else
+                squareSum y (larger x z)
+        squareSumGreater x y z
+    
+    let Point1_6_sqrt x y =
+        let square x :float =
+            x*x
+        let avg a b :float =
+            (a + b)/2.0
+        let abs x :float = 
+            if x < 0.0 then -x
+            else x
+        let improve guess x = 
+            avg guess (x / guess)
+        let goodEnough guess x =
+            abs((square guess) - x) < 0.001
+        let rec sqrt_first guess x =
+            if goodEnough guess x then guess
+            else sqrt_first (improve guess x) x
+
+        sqrt_first x y
+
+
---
+++ 1.2/mauricio.dominguez/Second.fs
@@ -0,0 +1,21 @@
+﻿module Second
+
+let rec factorial_Recursive x =
+    if x = 1 then 1
+    else x * (factorial_Recursive x - 1)
+
+
+let rec factorial_Sequential product counter maxCount =
+    if counter > maxCount then product
+    else factorial_Sequential (counter * product) (counter + 1) maxCount
+    
+let factorial x =
+    factorial_Sequential 1 1 x
+
+let rec A x y =
+    match x, y with
+        | x, 0 -> 0
+        | 0, y -> 2 * y
+        | x, 1 -> 2
+        | _ -> A (x - 1) (A x (y - 1))
+
